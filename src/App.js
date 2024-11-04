9672baf235c0c5ebd793037a47fdf49d15ee25bc
Mekhak70 <mexakchilingaryan3@gmail.com>
2024-11-04 11:11:03 +0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/header/header";
 import Section from "./components/section/section";
 
 function App() {
-  
   return (
     <div className={styles.container}>
       <div className={styles.content}>
