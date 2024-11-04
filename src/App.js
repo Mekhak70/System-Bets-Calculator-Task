d2273e8e3675fa4f8aebac84bb8288435b8795e3
Mekhak70 <mexakchilingaryan3@gmail.com>
2024-11-04 17:58:21 +0400

    bug fixd

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Section from "./components/section/section";
 function App() {
   return (
     <div className={styles.container}>
+    
       <div className={styles.content}>
         <Header />
         <Section />
